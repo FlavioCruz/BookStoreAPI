fcc4b261736cd620a09b0102593a9f60bf4c9922
FlavioCruz <flaviocoutinho.nave@gmail.com>
2018-01-31 00:52:03 -0200

    Comentários na BDDataAccess.cs

--- BookStoreAPI/BookStoreAPI/BookStoreDataAccess/Entities/DBDataAccess.cs
+++ BookStoreAPI/BookStoreAPI/BookStoreDataAccess/Entities/DBDataAccess.cs
@@ -12,30 +12,65 @@ namespace BookStoreAPI.BookStoreDataAccess.Entities
     {
         private BookStoreEntities db = new BookStoreEntities();
 
+        /// <summary>
+        /// Lista todas as ocorrencias
+        /// </summary>
+        /// <typeparam name="T">Modelo de dados</typeparam>
+        /// <param name="query">query a consulta ao banco</param>
+        /// <param name="args">lista de argumentos da consulta</param>
+        /// <returns>Lista de objetos</returns>
         public async Task<List<T>> ListAll<T>(string query, params object[] args)
         {
-            List<T> result = await db.Database.SqlQuery<T>(query).ToListAsync();
+            List<T> result = await db.Database.SqlQuery<T>(query, args).ToListAsync();
             return result;
         }
 
+        /// <summary>
+        /// Busca objeto pelo ID
+        /// </summary>
+        /// <typeparam name="T">Modelo de dados</typeparam>
+        /// <param name="query">query a consulta ao banco</param>
+        /// <param name="args">lista de argumentos da consulta</param>
+        /// <returns>Objeto</returns>
         public async Task<T> ListById<T>(string query, params object[] args)
         {
             var result = await db.Database.SqlQuery<T>(query, args).SingleOrDefaultAsync();
             return result;
         }
 
+        /// <summary>
+        /// Insere o objeto no banco
+        /// </summary>
+        /// <typeparam name="T">Modelo de dados</typeparam>
+        /// <param name="query">query a consulta ao banco</param>
+        /// <param name="args">lista de argumentos da consulta</param>
+        /// <returns>Objeto</returns>
         public async Task<T> InsertAndGetObj<T>(string query, params object[] args)
         {
             T result = await db.Database.SqlQuery<T>(query, args).SingleOrDefaultAsync();
             return result;
         }
 
+        /// <summary>
+        /// Atualiza o objeto no banco
+        /// </summary>
+        /// <typeparam name="T">Modelo de dados</typeparam>
+        /// <param name="query">query a consulta ao banco</param>
+        /// <param name="args">lista de argumentos da consulta</param>
+        /// <returns>Objeto</returns>
         public async Task<T> Update<T>(string query, params object[] args)
         {
             T result = await db.Database.SqlQuery<T>(query, args).SingleOrDefaultAsync();
             return result;
         }
 
+        /// <summary>
+        /// Deleta o objeto do banco
+        /// </summary>
+        /// <typeparam name="T">Modelo de dados</typeparam>
+        /// <param name="query">query a consulta ao banco</param>
+        /// <param name="args">lista de argumentos da consulta</param>
+        /// <returns>Objeto</returns>
         public async Task<T> Delete<T>(string query, params object[] args)
         {
             T result = await db.Database.SqlQuery<T>(query, args).SingleOrDefaultAsync();
